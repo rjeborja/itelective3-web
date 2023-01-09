9552f093d7f586ee10bc87fdcd5c8b5840fabcab
rjeborja <rjeborja@addu.edu.ph>
2023-01-09 23:11:07 +0800

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,7 @@
 module.exports = {
   content: ["./CustomCard.html"],
   theme: {
-    extend: {
-      colors: {
-        'darkblue': '#010022',
-      },
-      boxShadow: {
-        'pfp': '0 0 20px 0 rgba(0, 0, 0, 50)',
-
-      },
-      borderWidth: {
-        default: '1px',
-      },
-      spacing: {
-        '16': '4rem',
-      }
-    },
-    
+    extend: {},
   },
   plugins: [],
 }
